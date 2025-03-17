f6e1ab33fb3499e74a452ddf9d16623a7c180949
dayannne <dayoun202@gmail.com>
2025-03-17 03:02:56 +0000

    Chore: prettier, preset-px-to-rem 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import localFont from 'next/font/local';
+import './globals.css';
 
 const pretendard = localFont({
-  src: "../styles/font/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
+  src: '../styles/font/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
 });
 
 export default function RootLayout({
@@ -14,11 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${pretendard.variable}`}> 
-      <body className="bg-background font-pretendard"> 
-        <main>
-          {children}
-        </main>
+    <html lang='en' className={`${pretendard.variable}`}>
+      <body className='bg-background font-pretendard'>
+        <main>{children}</main>
       </body>
     </html>
   );
